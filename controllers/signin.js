c6f50ce6a1b4477bceb68fa2b8234b02eb0a762c
Sriram2475 <sriram.r240705@gmail.com>
2024-10-01 09:48:41 +0530

    Update signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -22,6 +22,7 @@ async function InsertVerifyUser(name, email, password) {
     });
 
     const activationLink = `http://localhost:4000/signin/${token}`;
+    const activationLink = `https://auth-backend-4aaf.onrender.com/signin/${token}`;
     const content = `<h4> hi,there </h4>
     <h5>Welcome to the app</h5>
     <p>Thank you for signingup.Click on the below link to activate</p>
